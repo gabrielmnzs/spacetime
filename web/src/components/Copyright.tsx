a60bb0a3ef17577af4182199a36be7be249e1f14
Gabriel Menezes <gabrielmenezes@MacBook-Pro-de-Gabriel.local>
2023-07-12 21:31:36 -0300

    :hammer: refactor: split main page in components

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NWL da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://rocketseat.com.br"
+        className="hover: text-gray-100 underline
+  "
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
